270daa6310a69b36b2138c606562213e4767bb05
Justin Ward <jward@modernmagnetic.com>
2015-05-06 14:51:58 +1000

    code format cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,3 @@
 Bar.create(name: 'Cock and Hound', address: '18 Peck Terrace', suburb: 'Williams Landing', state: 'Vic', postcode: '3027', contact_1: 'www.ford.com', contact_2: '123456789', review: '4/5')
 Bar.create(name: 'Pig in Spit', address: '15 Lonsdale Street', suburb: 'Melbourne', state: 'Victoria', postcode: '3000', contact_1: 'www.google.com', contact_2: '9876543', review: '4.5/5')
 Bar.create(name: 'Royal', address: '12 Rennison Street', suburb: 'Horsham', state: 'vic', postcode: '3400', contact_1: 'www.toyota.com', contact_2: '12256789', review: '2.5/5')
-
